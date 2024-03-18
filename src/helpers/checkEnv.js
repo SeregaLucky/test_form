d085c6695110f7b2ecae53235ffc908e5f37fa21
SeregaLucky <strizgsergey@gmail.com>
2024-03-18 04:51:40 +0200

    small fix in env

--- src/helpers/checkEnv.js
+++ src/helpers/checkEnv.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 export const currentRedirectUrl = isProduction
-  ? process.env.REACT_APP_BASE_PRODUCT_URL
+  ? process.env.REACT_APP_BASE_PRODUCTION_URL
   : process.env.REACT_APP_BASE_LOCAL_URL;
