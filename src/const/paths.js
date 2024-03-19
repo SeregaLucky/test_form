1a5a69a48ec408b252e13d7cd1e6e3fb35783e25
SeregaLucky <strizgsergey@gmail.com>
2024-03-19 17:40:40 +0200

    add additionally jsDoc for paths.js

--- src/const/paths.js
+++ src/const/paths.js
@@ -1,8 +1,19 @@
 /**
  * An object containing links to various page routes
  * @typedef {Object} RoutePaths
- * @property {Object} HOME - Link to main page
- * @property {Object} AUTH - Links to auth route
+ * @property {Object} HOME - data for link
+ * @property {string} HOME.path - Link to main page
+ * @property {Object} AUTH - data for link
+ * @property {string} AUTH.path - Link to auth route
+ * @property {Object} AUTH.children - children auth pages
+ * @property {Object} AUTH.children.LOGIN - data for link
+ * @property {string} AUTH.children.LOGIN.path - Link to login page
+ * @property {Object} AUTH.children.FORGOT_PASSWORD - data for link
+ * @property {string} AUTH.children.FORGOT_PASSWORD.path - Link to forgot-password page
+ * @property {Object} AUTH.children.RESET_PASSWORD - data for link
+ * @property {string} AUTH.children.RESET_PASSWORD.path - Link to reset-password page
+ * @property {Object} AUTH.children.REGISTRATION - data for link
+ * @property {string} AUTH.children.REGISTRATION.path - Link to registration page
  */
 
 /**
